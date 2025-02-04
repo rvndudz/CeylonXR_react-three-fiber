553358ea6d87a26098666d559b1d8b213c3fc46b
rvndudz <rvndudz77@gmail.com>
2025-02-04 14:41:45 +0530

    integrate react-router-dom for routing and add dynamic scene page support

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/UI/About";
 import Hero from "./components/UI/Hero";
 import NavBar from "./components/UI/Navbar";
 import Places from "./components/UI/Places";
 import Contact from "./components/UI/Contact";
 import Footer from "./components/UI/Footer";
+import ScenePage from "./components/UI/ScenePage";
 
 function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
-      <Hero />
-      <About />
-      <Places />
-      <Contact />
-      <Footer />
-    </main>
+    <Router>
+      <Routes>
+        {/* Main landing page route */}
+        <Route
+          path="/"
+          element={
+            <main className="relative min-h-screen w-screen overflow-x-hidden">
+              <NavBar />
+              <Hero />
+              <About />
+              <Places />
+              <Contact />
+              <Footer />
+            </main>
+          }
+        />
+        {/* Dynamic route for scene pages */}
+        <Route path="/:sceneName" element={<ScenePage />} />
+      </Routes>
+    </Router>
   );
 }
 
