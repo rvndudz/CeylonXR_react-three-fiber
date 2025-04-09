b666589249f02cfd0c87dace087b9b4e36673ab0
rvndudz <rvndudz77@gmail.com>
2025-04-09 12:38:55 +0530

    fix: update file reference in KSplatViewer to use test3.ksplat

--- src/components/KSplatViewer.jsx
+++ src/components/KSplatViewer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Viewer, KSplatLoader } from "../GS-Engine";
 
 const KSplatViewer = () => {
-  const file = "/ella.ksplat";
+  const file = "/test3.ksplat";
   const baseUrl =
     import.meta.env.MODE === "development"
       ? "/models"
